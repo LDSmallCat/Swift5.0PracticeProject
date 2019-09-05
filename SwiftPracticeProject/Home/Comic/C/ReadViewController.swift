044bff2ac27b60a9aadac59e2d218b1c52b4b5f6
cld <1026190993@qq.com>
2019-09-05 17:22:01 +0800

    no message

---
+++ SwiftPracticeProject/Home/Comic/C/ReadViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ReadViewController.swift
+//  SwiftPracticeProject
+//
+//  Created by Mac on 2019/9/5.
+//  Copyright © 2019 caolaidong. All rights reserved.
+//
+
+import UIKit
+
+class ReadViewController: LDBaseViewController {
+
+}
